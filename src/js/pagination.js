037b1b14403cf95ce7e8e81a9685f85f15900754
Oleksandr Kryvoniuk <sasha12887@gmail.com>
2022-11-30 01:10:02 +0200

    worked pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,11 +1,29 @@
 import Pagination from 'tui-pagination';
 import FilmApi from './movieAPI';
+import { createMarkUp } from './createMarkUp';
 
 const container = document.getElementById('tui-pagination-container');
+const filmAPI = new FilmApi();
+const ulEl = document.querySelector('.films');
+let currentPage = 1;
+
 const instance = new Pagination(container, {
-  totalItems: 10000,
-  itemsPerPage: 10,
+  totalItems: 20000,
+  itemsPerPage: 20,
   visiblePages: 5,
-  centerAlign: true
+  page: currentPage,
+  centerAlign: false,
 });
-instance.getCurrentPage();
+
+instance.on('afterMove', (event) => {
+  ulEl.replaceChildren([]);
+  currentPage = event.page;
+
+  filmAPI
+  .getPopularFilms(currentPage)
+  .then(({ page, results, total_pages, total_results }) => {
+    const markUp = createMarkUp(results);
+    ulEl.insertAdjacentHTML('beforeend', markUp);
+  })
+  .catch(err => console.log(err.message));
+});
