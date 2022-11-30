4deb389c7b95809694596c008d40338c462415f2
Yuliia Dikun <dikun.yulia@gmail.com>
2022-11-30 19:47:33 +0200

    add render film modal

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -1,18 +1,30 @@
+import Notiflix from 'notiflix';
+import FilmApi from './movieAPI';
+import filmCard from '../templates/modal-film.hbs';
+
 const ulEl = document.querySelector('.films');
 const modal = document.querySelector('.modal-backdrop');
 const closeBtn = document.querySelector('.modal-close-btn');
-
 ulEl.addEventListener('click', onFimlsListClick);
+const filmAPIByID = new FilmApi();
 
-function onFimlsListClick(evt) {
-  console.log(evt.target.nodeName);
-  if (evt.target.nodeName === 'UL') {
-    return;
+async function onFimlsListClick(evt) {
+  try {
+    console.log(evt.target.nodeName);
+    if (evt.target.nodeName === 'UL') {
+      return;
+    }
+    let id = evt.target.closest('li').dataset.id;
+    filmAPIByID.idFilm = id;
+    const film = await filmAPIByID.getFilmByID();
+    const filmMarkUp = filmCard(film);
+    document.querySelector('body').insertAdjacentHTML('beforeend', filmMarkUp);
+
+    console.log(closeBtn);
+  } catch (error) {
+    Notiflix.Notify.failure(error.message);
   }
-
-  modal.classList.remove('is-hidden');
 }
-
 closeBtn.addEventListener('click', () => {
   modal.classList.add('is-hidden');
 });
