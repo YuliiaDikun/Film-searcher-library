050615adadf526f1c9802f980e243c328c838915
Vitalii-Bilobrov <106316488+Vitalii-Bilobrov@users.noreply.github.com>
2022-12-01 15:28:38 +0200

    corrected listeners

--- src/js/modal-open.js
+++ src/js/modal-open.js
@@ -3,8 +3,7 @@ import FilmApi from './movieAPI';
 import filmCard from '../templates/modal-film.hbs';
 
 const ulEl = document.querySelector('.films');
-const modal = document.querySelector('.modal-backdrop');
-const closeBtn = document.querySelector('.modal-close-btn');
+
 ulEl.addEventListener('click', onFimlsListClick);
 const filmAPIByID = new FilmApi();
 
@@ -18,13 +17,29 @@ async function onFimlsListClick(evt) {
     filmAPIByID.idFilm = id;
     const film = await filmAPIByID.getFilmByID();
     const filmMarkUp = filmCard(film);
+
     document.querySelector('body').insertAdjacentHTML('beforeend', filmMarkUp);
 
+    let modal = document.querySelector('.modal-backdrop');
+    let closeBtn = document.querySelector('.modal-close-btn');
+
+    modal.addEventListener('click', event => {
+      if (event.target.nodeName === 'path' || event.target.nodeName === 'svg') {
+        console.log(event.target.nodeName);
+        modal.remove();
+      }
+      if (event.target === event.currentTarget) {
+        modal.remove();
+      }
+      if (event.target.dataset.name === 'watched') {
+        console.log('watched');
+      }
+      if (event.target.dataset.name === 'queue') {
+        console.log('queue');
+      }
+    });
     console.log(closeBtn);
   } catch (error) {
     Notiflix.Notify.failure(error.message);
   }
 }
-closeBtn.addEventListener('click', () => {
-  modal.classList.add('is-hidden');
-});
