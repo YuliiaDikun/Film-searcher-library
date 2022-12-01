a9d0fee313f451818e1feb9317dde9b6b32a11fb
Andriy Savych <andriysavych@gmail.com>
2022-12-01 02:20:23 +0100

    fixed
    тільки ще якийсь затуп стався з кліком по бекдропу... може підкажеш? я закоментував...бо тоді якийсь трабл...

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,17 +1,40 @@
 const openModal = document.querySelector('.js-team-link');
 const modalWindow = document.querySelector('#js-modal')
 const btnClose = document.querySelector('#modal-btn')
+const modalTeam = document.querySelector('#js-modal-team')
 
-const onTeamClick = (event) => {
+const onModalClick = (event) => {
   modalWindow.classList.remove('visually-hidden');
-}
+  document.body.style.overflow = "hidden";
 
-openModal.addEventListener('click', onTeamClick)
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      modalWindow.classList.add('visually-hidden');
+      document.body.style.overflow = "auto";
+      document.removeEventListener('keydown', onModalClick);
+    }
+  });
+
+    //   window.onclick = () => {
+    //   console.log(event.target);
+    //   if (event.target !== modalTeam) {
+    //     modalWindow.classList.add('visually-hidden')
+    //   };
+    // };
+};
+
+openModal.addEventListener('click', onModalClick)
+
 
 
 const onCloseBtn = () => {
   modalWindow.classList.add('visually-hidden');
+  document.body.style.overflow = "auto";
+  document.removeEventListener('keydown', onModalClick);
 }
 
 btnClose.addEventListener('click', onCloseBtn)
+
+
+
 
