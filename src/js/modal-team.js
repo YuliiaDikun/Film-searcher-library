31e3cd4993178f271ce2671bc6c3435557e3c6f6
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-04 15:28:56 +0200

    fix footer modal

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -5,11 +5,18 @@ const btnClose = document.querySelector('#modal-btn');
 const modalTeam = document.querySelector('#js-modal-team');
 
 const close = event => {
-  console.log(event);
   if (event.code === 'Escape') {
     modalWindow.classList.add('is-hidden');
     document.body.style.overflow = 'auto';
     document.removeEventListener('keydown', close);
+  }
+};
+const onModalWindow = event => {
+  if (event.target === event.currentTarget) {
+    modalWindow.classList.add('is-hidden');
+    document.body.style.overflow = 'auto';
+    document.removeEventListener('keydown', close);
+    modalWindow.removeEventListener('click', onModalWindow);
   }
 };
 
@@ -18,6 +25,7 @@ const onModalClick = event => {
   document.body.style.overflow = 'hidden';
 
   document.addEventListener('keydown', close);
+  modalWindow.addEventListener('click', onModalWindow);
 };
 
 const onCloseBtn = () => {
