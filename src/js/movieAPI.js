73047c77de9bee79e4c6820e78209c9471821c6f
Yuliia Dikun <dikun.yulia@gmail.com>
2022-12-04 15:53:53 +0200

    add team memder photo and fix typos

--- src/js/movieAPI.js
+++ src/js/movieAPI.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-
+const API_KEY = 'f9c0bb738fb5ed39704a871786e56353';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 export default class FilmApi {
-  #API_KEY = 'f9c0bb738fb5ed39704a871786e56353';
   constructor() {
     this.query = '';
     this.id = null;
@@ -16,31 +15,29 @@ export default class FilmApi {
   }
   async getPopularFilms(page = 1) {
     const { data } = await axios.get(
-      `/trending/movie/week?api_key=${this.#API_KEY}&page=${page}`
+      `/trending/movie/week?api_key=${API_KEY}&page=${page}`
     );
     return data;
   }
   async getFilmByID() {
-    const { data } = await axios.get(
-      `/movie/${this.id}?api_key=${this.#API_KEY}`
-    );
+    const { data } = await axios.get(`/movie/${this.id}?api_key=${API_KEY}`);
     return data;
   }
   async getFilmByQuery(page = 1) {
     const { data } = await axios.get(
-      `/search/movie?api_key=${this.#API_KEY}&query=${this.query}&page=${page}`
+      `/search/movie?api_key=${API_KEY}&query=${this.query}&page=${page}`
     );
     return data;
   }
   async getGenres() {
     const { data } = await axios.get(
-      `/genre/movie/list?api_key=${this.#API_KEY}&language=en-US`
+      `/genre/movie/list?api_key=${API_KEY}&language=en-US`
     );
     return data;
   }
   async getTrailerById() {
     const { data } = await axios.get(
-      `/movie/${this.id}/videos?api_key=${this.#API_KEY}&language=en-US`
+      `/movie/${this.id}/videos?api_key=${API_KEY}&language=en-US`
     );
     return data;
   }
